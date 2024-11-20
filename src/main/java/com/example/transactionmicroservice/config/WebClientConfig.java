70affc9091c74b92b8198b2cc34ee609fc59d0c1
brigittemendez1302 <u201914955@upc.edu.pe>
2024-11-20 12:38:05 -0500

    add: microservice path in properties

--- src/main/java/com/example/transactionmicroservice/config/WebClientConfig.java
+++ src/main/java/com/example/transactionmicroservice/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.example.transactionmicroservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -11,6 +12,12 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     /**
+     * Base URL for the Account Microservice, injected from properties.
+     */
+    @Value("${microservices.bank-accounts.base-url}")
+    private String bankAccountsBaseUrl;
+
+    /**
      * Creates a WebClient bean with a base URL for the Account Microservice.
      *
      * @return A configured WebClient instance.
@@ -18,7 +25,7 @@ public class WebClientConfig {
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
         return builder
-                .baseUrl("http://localhost:8082/api") // Base URL for the Account Microservice
+                .baseUrl(bankAccountsBaseUrl) // Base URL for the Account Microservice
                 .build();
     }
 }
